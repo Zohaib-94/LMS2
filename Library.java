bfc1deebdd1ee89d1c31ef92ecc0f8ce1900a1bc
Zohaib-94 <143363565+Zohaib-94@users.noreply.github.com>
2023-10-08 23:53:26 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,36 +1,66 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+
 
 
 public class Library {
-    private List<Book> books;
-
-
-    public Library() {
-        books = new ArrayList<>();
-    }
-
+    private List<Book> books = new ArrayList<>();
 
     public void addBook(Book book) {
         books.add(book);
     }
 
-
-    public void removeBookById(String id) {
-        for (Book book : books) {
-            if (book.getId().equals(id)) {
-                books.remove(book);
-                return;
+    public boolean removeBookByBarcode(String barcode) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getBarcode().equals(barcode)) {
+                iterator.remove();
+                return true;
             }
         }
+        return false;
     }
 
+    public boolean removeBookByTitle(String title) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
 
-    public void listAllBooks() {
+    public Book findBookByTitle(String title) {
         for (Book book : books) {
-            System.out.println("ID: " + book.getId() + ", Title: " + book.getTitle() + ", Author: " + book.getAuthor() + ", Available: " + book.isAvailable());
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void checkOutBook(String title) {
+        Book book = findBookByTitle(title);
+        if (book != null && book.isAvailable()) {
+            book.setAvailable(false);
         }
     }
 
+    public void checkInBook(String title) {
+        Book book = findBookByTitle(title);
+        if (book != null && !book.isAvailable()) {
+            book.setAvailable(true);
+        }
+    }
 
+    public void printLibrary() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
 }
