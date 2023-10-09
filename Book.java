bd1b688bf70f58d6187c6c2dae74b96a62bbdf91
Zohaib-94 <143363565+Zohaib-94@users.noreply.github.com>
2023-10-08 23:52:29 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,25 +1,23 @@
 // Zohaib Ahmadzai
 // CEN-3024C
-// 9/10/2023
+// 10/08/2023
 
-// Created book class with ID, Title and Author
+// Created book class with Barcode, Title and Author
 public class Book {
-    private String id;
+    private String barcode;
     private String title;
     private String author;
     private boolean available;
 
-
-    public Book(String id, String title, String author) {
-        this.id = id;
+    public Book(String barcode, String title, String author) {
+        this.barcode = barcode;
         this.title = title;
         this.author = author;
         this.available = true;
     }
 
-
-    public String getId() {
-        return id;
+    public String getBarcode() {
+        return barcode;
     }
 
     public String getTitle() {
@@ -34,8 +32,12 @@ public class Book {
         return available;
     }
 
-
     public void setAvailable(boolean available) {
         this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Barcode: " + barcode + " | Title: " + title + " | Author: " + author + " | Available: " + available;
     }
 }
